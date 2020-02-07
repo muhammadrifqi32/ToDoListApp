29faef0abdc565189d9a5e3a55ae9a1d9c347289
muhammadrifqi32 <33801152+muhammadrifqi32@users.noreply.github.com>
2020-02-07 17:23:56 +0700

    adding checking username and email at register

--- ToDoList/ToDoList/Controllers/UserController.cs
+++ ToDoList/ToDoList/Controllers/UserController.cs
@@ -77,7 +77,7 @@ namespace ToDoList.Controllers
 
 
         [HttpPost]
-        public JsonResult Register(UserVM userVM)
+        public ActionResult Register(UserVM userVM)
         {
             userVM.Password = Hashing.HashPassword(userVM.Password);
             var client = new HttpClient
@@ -105,11 +105,11 @@ namespace ToDoList.Controllers
                 //smtp.Credentials = nc;
                 //smtp.Send(mm);
                 //ViewBag.Message = "Password Has Been Sent.Check your email to login";
-                return Json(result);
+                return RedirectToAction("Login");
             }
             else
             {
-                return Json(result);
+                return View();
             }
         }
 
